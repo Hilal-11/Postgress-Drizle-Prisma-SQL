c86abfc6d9affc85e9c6a7e9d5639d95b4bd044d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-13 01:01:30 +0530

    prisma database fro sql with neon

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,12 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient();
+
+
 const registerUser = async (req , res ) => {
     console.log("register controller done")
+    prisma.user.findUnique({
+        where: { email }
+    })
 }
 
-module.exports = registerUser
+export default registerUser
