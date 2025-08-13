29c5d035181f6d67a8cc5501baa869784bb515b2
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-13 23:56:24 +0530

    done

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,6 +1,20 @@
-
+import { PrismaClient } from "@prisma/client/extension"
+const prisma = PrismaClient()
 const profile = async (req , res) => {
+    const { userId } = req.user.id
     try{
+        const user = await prisma.User.findUnique({ where: { userId } })
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "user info",
+            response: user,
+        })
         
     }catch(error) {
         console.log(error.message)
