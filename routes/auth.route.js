b8e8918c379e1f480bc77c845da9c5aafd55137a
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-13 22:21:43 +0530

    done routers, api end points and controllers[Handlers]

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,10 +2,19 @@ import express from "express"
 const userRouter = express.Router();
 
 import register from "../controllers/register.controller.js"
+import varify from '../controllers/varify.controller.js'
+import login from '../controllers/login.controller.js'
+import profile from '../controllers/profile.controller.js'
+import forgetPassword from '../controllers/forgetPassword.controller.js'
+import resetPassword from '../controllers/resetPassword.controller.js'
+import logout from '../controllers/logout.controller.js'
 
 router.post("/register" , register);
-router.post("/register" , register);
-router.post("/register" , register);
-
+router.post("/varify/:token" , varify);
+router.post("/login" , login);
+router.get("/profile" , profile);
+router.post("/forgetPassword" , forgetPassword);
+router.post("/resetPassword" , resetPassword);
+router.get("/logout", logout);
 
 export default userRouter
