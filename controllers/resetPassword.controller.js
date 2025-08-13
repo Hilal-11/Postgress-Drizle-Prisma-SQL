c72c2d1f637601ad8512bc7fc05bf403c76e9f17
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-08-13 22:14:06 +0530

    backend with sql

---
+++ controllers/resetPassword.controller.js
@@ -0,0 +1,14 @@
+
+const resetPassword = async (req , res) => {
+    try{
+        
+    }catch(error) {
+        console.log(error.message)
+        return res.status(400).json({
+            success: false,
+            message: "",
+            error: error.message
+        })
+    }
+}
+export default resetPassword
